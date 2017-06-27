803bf040976d67fb605f191793d030dbb94c9cf3
howardmann <howardmann27@gmail.com>
2017-06-27 22:25:07 +0200

    Add destroy action

--- app/routes/movies/movie.js
+++ app/routes/movies/movie.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('movie', params.movie_id);
+  },
+  actions: {
+    deleteMovie(movie){
+      movie.destroyRecord().then(()=> this.transitionTo('movies'));
+    }
   }
 });
